b0956b653acc550e2ed0a41b1d5eb03eb37858bd
Charanvir <charanvir123@gmail.com>
2022-04-13 03:45:04 -0400

    added more questions to the inquirer prompt
    Added more to the markdown template

--- develop-tools/generateMarkdown.js
+++ develop-tools/generateMarkdown.js
@@ -1,7 +1,47 @@
 // Page to create function which generates the markdown
 module.exports = data => {
     return `# ${data.title}
-## Testing
+
+![badmath](https://img.shields.io/badge/License-${data.license}-green)
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+The following application is covered under the ${data.license} License
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+[Github](https://github.com/${data.github})
+
+
+Direct any additional questions you have regarding this application to ${data.email}
+
 
     `
 }
--- index.js
+++ index.js
@@ -18,6 +18,103 @@ const promptUser = () => {
                         return false
                     }
                 }
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Please enter your Github username',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter your email for users to contact you",
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: "license",
+                message: "What license would you like to have you project use?",
+                choices: ["MIT", "GNU_AGPLv3", "Apache_2.0", "BSD", "ISC"],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Please provide a description explaining your application. You should state your motivation behind building it, the prolem that is solved, why you built it and more.',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Please provide the user how to install your application',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Please provide the user how to use your application, the more specific the better as it will help the user get the best experience out of our application.',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Please provide how other developers can contribute to your application.',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Please provide how to test your application',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter a name of your project")
+                        return false
+                    }
+                }
             }
         ])
 }
